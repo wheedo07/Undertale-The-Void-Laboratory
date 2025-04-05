5752a41b36c280bec22b331c0080fe3a3c17de9a
brillianintdoh <wheedo07@ourgram.co.kr>
2025-04-05 18:43:08 +0900

    pixel 글자 정상화 및 대사 수정과 배틀

--- cpp/src/overworld/laboratoryNode/laboratoryNode.cpp
+++ cpp/src/overworld/laboratoryNode/laboratoryNode.cpp
@@ -42,9 +42,38 @@ void LaboratoryNode::init() {
             [this]() {
                 summontextbox()->character(Character::SANS, sys->dia()->from(
                     PackedStringArray({
-                        String::utf8("* ..?")
+                        String::utf8("* ..?"),
+                        String::utf8("* ..."),
+                        String::utf8("* 음 kid 여기 있었네.."),
+                        String::utf8("* 뼈밖에서 보니까 반갑네.."),
+                        String::utf8("* 내가 여기 있는것도 우연은 아닐거고... 그치?"),
                     })
-                )->set_speed(Array::make(0.03))->set_expressions(Array::make(10)));
+                )->set_expressions(Array::make(10, 18, 0, 1, 4))
+                ->set_speed(Array::make(0.03)));
+            },
+            [this]() {
+                summontextbox()->generic(sys->dia()->from(
+                    PackedStringArray({
+                        String::utf8("* (연구소는 평소와 다르게 음산하다...)")
+                    })
+                ));
+            },
+            [this]() {
+                summontextbox()->character(Character::SANS, sys->dia()->from(
+                    PackedStringArray({
+                        String::utf8("* 다른 타임라인에서 네가 한 일들..."),
+                        String::utf8("* 얼마나 많은 괴물들을 죽였지?"),
+                        String::utf8("* 얼마나 많은 타임라인을 망쳐놨지?"),
+                        String::utf8("* 나는 기억하고 있어 너가 수많은 리셋으로 이르킨 일들.."),
+                        String::utf8("* ..."),
+                        String::utf8("* 그렇기에 kid 한가지 조언을 할게 너를 위해서기도 해"),
+                        String::utf8("* 이제 그만두는게 좋을거야")
+                    })
+                )->set_expressions(Array::make(4, 5, 15, 17, 19, 4, 5))
+                ->set_speed(Array::make(0.03, 0.01, 0.05)));
+            },
+            [this]() {
+                sys->load_battle("res://Game/encounters/sans_1.tres",  Vector2(324, 323));
             }
         });
     }
